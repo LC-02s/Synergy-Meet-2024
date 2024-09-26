4f0bebe8fb66d949107facc03a690358e8a8afcd
ChanLee_KR <cl700120@gmail.com>
2024-09-27 00:10:47 +0900

    feat: use interval 훅 추가

--- src/shared/hooks/index.ts
+++ src/shared/hooks/index.ts
@@ -1,3 +1,4 @@
 export * from './useBreakpoint'
 export { default as useStopScroll } from './useStopScroll'
 export { default as useWindowEvent } from './useWindowEvent'
+export { default as useInterval } from './useInterval'
---
+++ src/shared/hooks/useInterval.ts
@@ -0,0 +1,32 @@
+import React from 'react'
+import { useIsomorphicLayoutEffect } from 'framer-motion'
+
+export default function useInterval(callback: () => void, tick: number, autoStart = true) {
+  const savedCallback = React.useRef(callback)
+  const intervalId = React.useRef<number | null>(null)
+
+  const clear = React.useCallback(() => {
+    if (intervalId.current) {
+      window.clearInterval(intervalId.current)
+      intervalId.current = null
+    }
+  }, [])
+
+  const start = React.useCallback(() => {
+    clear()
+    intervalId.current = window.setInterval(() => {
+      savedCallback.current()
+    }, tick)
+  }, [tick, clear])
+
+  useIsomorphicLayoutEffect(() => {
+    savedCallback.current = callback
+  }, [])
+
+  React.useEffect(() => {
+    if (autoStart) start()
+    return clear
+  }, [start, clear, autoStart])
+
+  return { start, clear }
+}
