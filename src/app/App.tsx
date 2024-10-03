66b765f24d2f0989912abf6e03a045fbe02669bd
ChanLee_KR <cl700120@gmail.com>
2024-10-03 13:28:40 +0900

    refactor: global style 이동

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,20 +1,15 @@
 import { Header, Dock } from '@/widget/header'
 import { Intro } from '@/widget/intro'
 import { Footer } from '@/widget/footer'
-import { Global } from '@emotion/react'
-import { globalStyle } from './App.style'
 import Provider from './Provider'
 
 export default function App() {
   return (
-    <>
-      <Global styles={globalStyle} />
-      <Provider>
-        <Header />
-        <Intro />
-        <Dock />
-        <Footer />
-      </Provider>
-    </>
+    <Provider>
+      <Header />
+      <Intro />
+      <Dock />
+      <Footer />
+    </Provider>
   )
 }
--- src/app/Provider.tsx
+++ src/app/Provider.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Global } from '@emotion/react'
 import { OverlayViewer, MediaProvider } from '@/shared/hooks'
+import { globalStyle } from './App.style'
 
 export default function Provider({ children }: React.PropsWithChildren) {
   return (
     <MediaProvider>
+      <Global styles={globalStyle} />
       {children}
       <OverlayViewer />
     </MediaProvider>
