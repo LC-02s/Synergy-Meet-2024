ede2b8e019b7cf078be0dd6bf540c03665ae42e5
ChanLee_KR <cl700120@gmail.com>
2024-10-03 13:27:58 +0900

    chore: lint 룰 재설정

--- eslint.config.js
+++ eslint.config.js
@@ -5,18 +5,22 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import react from 'eslint-plugin-react'
 import jsxA11y from 'eslint-plugin-jsx-a11y'
 import prettier from 'eslint-plugin-prettier'
-import tseslint from 'typescript-eslint'
+import typescriptEslint from 'typescript-eslint'
 import eslintConfigPrettier from 'eslint-config-prettier'
 
 export default [
-  ...tseslint.config(
+  ...typescriptEslint.config(
     { ignores: ['dist'] },
     {
-      extends: [js.configs.recommended, ...tseslint.configs.recommended],
+      extends: [js.configs.recommended, ...typescriptEslint.configs.recommended],
       files: ['**/*.{ts,tsx}'],
       languageOptions: {
         ecmaVersion: 2020,
         globals: globals.browser,
+        parserOptions: {
+          project: ['./tsconfig.node.json', './tsconfig.app.json'],
+          tsconfigRootDir: import.meta.dirname,
+        },
       },
       plugins: {
         react,
@@ -26,8 +30,16 @@ export default [
         prettier,
       },
       rules: {
-        ...reactHooks.configs.recommended.rules,
+        ...react.configs.recommended.rules,
+        ...react.configs['jsx-runtime'].rules,
+        'react/jsx-props-no-spreading': 'off',
+        'react/require-default-props': 'off',
+        'react/prop-types': 'off',
+        'react/display-name': 'off',
         'react/no-unknown-property': ['error', { ignore: ['css'] }],
+        ...reactHooks.configs.recommended.rules,
+        ...jsxA11y.configs.recommended.rules,
+        'jsx-a11y/label-has-associated-control': ['error', { some: ['nesting', 'id'] }],
         'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
         'prettier/prettier': 'warn',
       },
