e4729522a42d7bcaa87c4b80483fba11c18d7b00
ChanLee_KR <cl700120@gmail.com>
2024-11-24 14:57:35 +0900

    refactor: 메뉴 모바일 컴포넌트 분리

--- src/widget/header/ui/Menu.tsx
+++ src/widget/header/ui/Menu.tsx
@@ -12,50 +12,56 @@ function MenuItem(props: JSX.IntrinsicElements['a']) {
   )
 }
 
-function MenuRoot({ children }: React.PropsWithChildren) {
-  const md = useBreakpoint('md')
+function MenuMobile({ children }: React.PropsWithChildren) {
   const [isOpen, { setFalse: close, toggle }] = useBooleanState()
   const containerRef = useOutsideClick<HTMLDivElement>(close)
 
-  React.useEffect(() => (!md ? close : undefined)?.(), [md, close])
+  React.useEffect(() => close, [close])
+
+  return (
+    <div ref={containerRef} css={menuRootStyleMd}>
+      <button type="button" title={isOpen ? '메뉴 닫기' : '메뉴 열기'} onClick={toggle}>
+        <p className={isOpen ? 'open' : ''}>
+          <span />
+          <span />
+          <span />
+        </p>
+        <HiddenText>{isOpen ? '메뉴 닫기' : '메뉴 열기'}</HiddenText>
+      </button>
+      <AnimatePresence>
+        {isOpen && (
+          <motion.nav
+            initial={{
+              transform: 'translate(8%, -12%) scale(0.8)',
+              opacity: 0,
+            }}
+            animate={{
+              height: 'auto',
+              transform: 'translate(0%, 0%) scaleY(1)',
+              opacity: 1,
+            }}
+            exit={{
+              height: 0,
+              transform: 'translate(8%, -12%) scale(0.8)',
+              opacity: 0,
+            }}
+            transition={{ ease: 'easeInOut', duration: 0.16 }}
+          >
+            <ul>{children}</ul>
+          </motion.nav>
+        )}
+      </AnimatePresence>
+    </div>
+  )
+}
+
+function MenuRoot({ children }: React.PropsWithChildren) {
+  const md = useBreakpoint('md')
 
   if (md) {
-    return (
-      <div ref={containerRef} css={menuRootStyleMd}>
-        <button type="button" title={isOpen ? '메뉴 닫기' : '메뉴 열기'} onClick={toggle}>
-          <p className={isOpen ? 'open' : ''}>
-            <span />
-            <span />
-            <span />
-          </p>
-          <HiddenText>{isOpen ? '메뉴 닫기' : '메뉴 열기'}</HiddenText>
-        </button>
-        <AnimatePresence>
-          {isOpen && (
-            <motion.nav
-              initial={{
-                transform: 'translate(8%, -12%) scale(0.8)',
-                opacity: 0,
-              }}
-              animate={{
-                height: 'auto',
-                transform: 'translate(0%, 0%) scaleY(1)',
-                opacity: 1,
-              }}
-              exit={{
-                height: 0,
-                transform: 'translate(8%, -12%) scale(0.8)',
-                opacity: 0,
-              }}
-              transition={{ ease: 'easeInOut', duration: 0.16 }}
-            >
-              <ul>{children}</ul>
-            </motion.nav>
-          )}
-        </AnimatePresence>
-      </div>
-    )
+    return <MenuMobile>{children}</MenuMobile>
   }
+
   return (
     <nav>
       <ul css={menuRootStyle}>{children}</ul>
